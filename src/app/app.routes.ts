875da226e827c710fb93c736da7045fd2792fa36
idrazovic <idrazovic@gmail.com>
2024-08-08 12:31:00 +0200

    canMatch route guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
-import { Routes } from "@angular/router";
+import { inject } from "@angular/core";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { routes as usersRoutes } from "./users/users.routes";
 import { NotFoundComponent } from "./not-found/not-found.component";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random() > 0.5;
+    return shouldGetAccess
+        ? true
+        : new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
     { path: '', component: NoTaskComponent },
@@ -11,6 +20,7 @@ export const routes: Routes = [
         path: 'users/:userId',
         component: UserTasksComponent,
         children: usersRoutes,
+        canMatch: [dummyCanMatch],
         data: {
             message: 'Welcome to your tasks!'
         },
